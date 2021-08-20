3858cd12d6de7f949c56ca4ccfabe58ee99fc3a8
konmike <michalkonecny10@gmail.com>
2021-08-20 15:46:44 +0200

    Add about footer

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="uvodnik" class="section section--editorial">
+  <div class="main main--about">
     <article class="article article--editorial">
       <p>
         "Túžim sa vrátiť do srdca chvály<br />
@@ -26,84 +26,19 @@
         >.
       </p>
     </article>
-    <footer class="footer footer--editorial">
-      <div class="footer__wrap-link">
-        <a
-          href="https://www.youtube.com/c/srdcechvaly"
-          class="footer__link"
-          title="Youtube - Srdce chvály"
-        >
-          <img
-            src="http://srdcechvaly.cekuj.net/wp-content/uploads/2019/04/channel.png"
-            class="footer__img"
-            alt="Youtube"
-          />
-          <h3 class="footer__title">Youtube kanál</h3>
-        </a>
-      </div>
-      <div class="footer__wrap-link">
-        <a
-          href="https://1drv.ms/f/s!AlC2uXdxwQQAhzi3uv_ykp9he49I"
-          class="footer__link"
-          title="Titulky ke stažení"
-        >
-          <img
-            src="http://srdcechvaly.cekuj.net/wp-content/uploads/2019/04/subtitle.png"
-            class="footer__img"
-            alt="Titulky"
-          />
-          <h3 class="footer__title">Titulky</h3>
-        </a>
-      </div>
-      <a
-        href="#home"
-        id="up"
-        class="button-up tlacitko"
-        style="display: block"
-      ></a>
-    </footer>
-  </section>
+    <about-footer />
+  </div>
 </template>
 
 <script>
-// import LastVideos from "@/components/LastVideos.vue";
+import AboutFooter from "@/components/AboutFooter.vue";
 export default {
   components: {
-    // LastVideos,
+    AboutFooter,
   },
   data() {
-    return {
-      // query: "",
-      // queryResults: [],
-      // videos: [],
-    };
+    return {};
   },
-  methods: {
-    getData() {
-      // console.log(this.query);
-      // this.queryResults = this.videos.filter((video) =>
-      //   video.title.toLowerCase().includes(this.query.toLowerCase())
-      // );
-      // console.log(this.queryResults[0]);
-      // `https://youtube.googleapis.com/youtube/v3/search?part=snippet
-      //  &channelId=UCQnju4UrTI_MN14nEtjxrjA
-      //  &q=${encodeURI(this.query)}
-      //  &key=AIzaSyBDHrY2FBFcdwk0OStWbBW4pYjT6cJKj3E`
-      //key = AIzaSyBDHrY2FBFcdwk0OStWbBW4pYjT6cJKj3E
-      //playlistUploadsId = UUQnju4UrTI_MN14nEtjxrjA
-      //channelId = UCQnju4UrTI_MN14nEtjxrjA
-      // axios
-      //   .get(
-      //     // `https://www.googleapis.com/youtube/v3/channels?id=UCQnju4UrTI_MN14nEtjxrjA&key=AIzaSyBDHrY2FBFcdwk0OStWbBW4pYjT6cJKj3E&part=contentDetails`
-      //     `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUQnju4UrTI_MN14nEtjxrjA&key=AIzaSyBDHrY2FBFcdwk0OStWbBW4pYjT6cJKj3E&part=snippet&order=date&maxResults=500`
-      //   )
-      //   .then((response) => {
-      //     console.log(response.data);
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
-    },
-  },
+  methods: {},
 };
 </script>
