1f33f27a6613ac25830f3fb22bbaa56f15004a49
Michal Konecny <konecnym@prf.cuni.cz>
2022-01-19 12:08:26 +0100

    Remove hash from URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import All from "../views/All.vue";
 import Search from "../views/Search.vue";
@@ -31,7 +31,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
